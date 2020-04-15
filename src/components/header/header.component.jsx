51a2a10ac6d9a8f4fef873567c9c3ea4caa1a562
robertos301 <robert.bojtos98@gmail.com>
2020-04-15 18:37:58 +0300

    implemented cart, cart-dropdown, and added collection-item styling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,9 @@ import './header.styles.scss';
 import {Link} from 'react-router-dom'
 import {ReactComponent as Logo} from '../../assets/book.svg';
 import {auth} from '../firebase/firebase.utils';
-
-const Header = ({currentUser})=> (
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+const Header = ({currentUser,hidden})=> (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -20,13 +21,18 @@ const Header = ({currentUser})=> (
                 :
                 <Link className='option' to='signin'>SIGN IN</Link>
             }
-
+            <CartIcon />
         </div>
+        {
+        hidden ? null : 
+        <CartDropdown/>
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart :{hidden}}) => ({
+    currentUser,
+    hidden
 })
 
 
