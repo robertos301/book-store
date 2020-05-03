0a7f9c7d863cb6bf46d91bb0003d691900d7222c
robertos301 <robert.bojtos98@gmail.com>
2020-05-03 20:35:14 +0300

    made the app load the data from each category in part, not just the shop component. Added selectors

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,29 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = (collectionMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTION,
-    payload: collectionMap
+import {firestore, convertCollectionSnapshotToMap} from '../../components/firebase/firebase.utils'; 
+
+export const fetchCollectionStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap
 })
+
+export const fetchCollectionFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+})
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionStart());
+
+        collectionRef.get().then(snapshot => {
+            const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+           dispatch(fetchCollectionsSuccess(collectionsMap));
+        }).catch(error => dispatch(fetchCollectionFailure(error.message)));
+    }
+}
